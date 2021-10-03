439462fdec8f39a5749c027b701b5bb0e3418232
MrWeber94 <abdelkareemelsayed2014@gmail.com>
2021-10-03 16:01:15 +0200

    v10.3

---
+++ index.php
@@ -0,0 +1,205 @@
+<?php 
+
+
+     //   echo   "test Message"; 
+     #   echo    "test Message"; 
+    
+     /*
+    echo   "test Message";  
+    echo   "test Message";  
+    echo   "test Message";  
+    echo   "test Message";    
+   */
+
+
+     // this line used to print message to user ... 
+    //   echo   "test Message"; 
+    //   print  ("test Message");
+
+
+
+   // ECHO   "test Message"; 
+
+
+    //    $age = 20;
+   //     echo $AGE;
+
+
+
+    //      $age =  30;
+    //      $name = "Root";
+
+    //    echo    var_dump($name);
+
+
+    // $age = 20;
+
+
+    // $age = 22;
+
+    // echo $age;     // 22   
+
+
+    // define("age",35,true);    // case-insensitive  by default  false    
+
+    // echo AGE;
+
+    //echo constant('age');
+
+
+
+    // $num1  = 2;
+    // $num2  = '2';
+
+
+      // echo  ($num1 + $num2);   // +,-,*,/,**,%
+
+      // echo   $num1 += $num2;    // ==  $num1 = $num1+$num2
+
+
+   
+
+    //   $num1 == $num2     true   
+
+    //   $num1 === $num2    false 
+
+        //   $num1 != $num2 
+        //   $num1 !==  $num2 
+
+
+        // >
+        // < 
+        // >=
+        // <=  
+
+
+
+     //   $num = 1;
+
+        // $num = $num + 1; 
+
+        // $num +=1;
+          //echo      $num++;   // 1
+        //  echo      ++$num;   // 2 
+ 
+
+        //   $num = $num - 1; 
+
+        //   $num -=1;
+            //    $num--;   // 
+            //    echo  $num;
+      //   echo      --$num;   // 2 
+
+/*
+    cond1       op          cond2      result 
+     true       and &&       true       true 
+     true       and          false     false 
+     false      and          true      false 
+     false      and          false     false 
+
+
+     
+    cond1       op          cond2      result 
+    true        or ||       true       true 
+    true        or          false      true  
+    false       or          true       true  
+    false       or          false      false 
+
+
+    cond1       op          cond2      result 
+    true        xor          true       false 
+    true        xor          false      true  
+    false       xor          true       true  
+    false       xor          false      false 
+
+
+    !(true) = flase 
+
+*/
+
+
+
+
+//   $fName = "Root";
+//   $LName = "Test";
+
+//     $FullName = $fName.'<br>'.$LName;
+
+//     echo $FullName;
+
+  
+    //  $flag = 3;
+
+     
+    //   if($flag == 1){
+
+    //     echo 'Flag = 1';
+
+    //   }elseif($flag == 2){
+
+    //     echo 'Flag = 2';
+
+    //   }elseif($flag == 3){
+
+    //     echo 'Flag = 3';
+
+    //   }elseif($flag == 4){
+
+    //     echo 'Flag = 4';
+
+    //   }else{
+    //       echo 'Flag Value = '.$flag;
+    //   }
+
+    // $flag = true;
+
+    // if($flag){
+    //     echo 'messge';
+    // }
+
+
+
+    // $color = "red";
+
+    //    switch ($color) {
+    //        case 'green':
+    //            # code...
+    //            echo 'green';
+    //            break;
+
+    //            case 'red':
+    //             # code...
+    //             echo 'red';
+    //             break;
+                
+    //             case 'blue':
+    //                 # code...
+    //                 echo 'blue';
+    //                 break;    
+           
+    //        default:
+    //            # code...
+    //            echo 'NO choice';
+    //            break;
+    //    }
+
+
+
+    // $age =20;
+
+
+    //  switch ($age) {
+    //      case ($age > 10 ):
+    //          # code...
+    //          echo 'age > 10';
+    //          break;
+         
+    //      default:
+    //          # code...
+    //          echo 'age < 10';
+    //          break;
+    //  }
+
+?>
+
+
